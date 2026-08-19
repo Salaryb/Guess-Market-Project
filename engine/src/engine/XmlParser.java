9650dcb23f1f78237b9a1f6faf097b92c74337c5
salaryb <ben.jaitin@mail.huji.ac.il>
2026-08-19 10:15:46 +0300

    Add xsd file schema checker

--- engine/src/engine/XmlParser.java
+++ engine/src/engine/XmlParser.java
@@ -5,25 +5,32 @@ import exception.GuessMarketException;
 
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.Unmarshaller;
+import javax.xml.XMLConstants;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
 import java.io.File;
 
 public class XmlParser {
-    public static GuessMarketData parse(String filePath) {
-        try {
-            File file = new File(filePath);
-            if (!file.exists()) {
-                throw new GuessMarketException("XML file not found: " + filePath);
-            }
 
+    public static GuessMarketData parse(String xmlFilePath) {
+        try {
             JAXBContext jaxbContext = JAXBContext.newInstance(GuessMarketData.class);
             Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 
-            GuessMarketData data = (GuessMarketData) unmarshaller.unmarshal(file);
+            SchemaFactory sf = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            File schemaFile = new File("GM-EX1-schema.xsd");
 
-            return data;
+            if (schemaFile.exists()) {
+                Schema schema = sf.newSchema(schemaFile);
+                unmarshaller.setSchema(schema);
+            } else {
+                throw new GuessMarketException("Schema file 'GM-EX1-schema.xsd' not found in the root directory.");
+            }
+
+            return (GuessMarketData) unmarshaller.unmarshal(new File(xmlFilePath));
 
         } catch (Exception e) {
-            throw new GuessMarketException("Failed to parse XML: " + e.getMessage());
+            throw new GuessMarketException("XML Error: " + e.getMessage());
         }
     }
 }
